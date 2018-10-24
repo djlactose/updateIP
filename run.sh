e202c8d18b074e16b10d36fe2f6354ddbe239265
Nick Hernandez <nickhernandez@djlactose.com>
2018-10-23 22:49:02 -0400

    Added updateip run on the container start

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ sed -i 's:key="":key=$cf_key:g' /etc/updateIP/updateIP.conf
 sed -i 's:id="":id=$cf_id:g' /etc/updateIP/updateIP.conf
 sed -i 's:zone="":zone=$cf_zone:g' /etc/updateIP/updateIP.conf
 sed -i 's:subdomain="":subdomain=$subdomain:g' /etc/updateIP/updateIP.conf
+/bin/updateip
 crond -f
