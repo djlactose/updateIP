c058bc252faf9921fb2c0b68597068ab13c88f03
Nick Hernandez <nickhernandez@djlactose.com>
2018-10-24 11:33:24 -0400

    Fixed for bug in run.sh where vars were not being replaced

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-sed -i 's:email="":email=$cf_email:g' /etc/updateIP/updateIP.conf
-sed -i 's:key="":key=$cf_key:g' /etc/updateIP/updateIP.conf
-sed -i 's:id="":id=$cf_id:g' /etc/updateIP/updateIP.conf
-sed -i 's:zone="":zone=$cf_zone:g' /etc/updateIP/updateIP.conf
-sed -i 's:subdomain="":subdomain=$subdomain:g' /etc/updateIP/updateIP.conf
+sed -i "s:email=\"\":email=$cf_email:g" /etc/updateIP/updateIP.conf
+sed -i "s:key=\"\":key=$cf_key:g" /etc/updateIP/updateIP.conf
+sed -i "s:id=\"\":id=$cf_id:g" /etc/updateIP/updateIP.conf
+sed -i "s:zone=\"\":zone=$cf_zone:g" /etc/updateIP/updateIP.conf
+sed -i "s:subdomain=\"\":subdomain=$subdomain:g" /etc/updateIP/updateIP.conf
 /bin/updateip
 crond -f
