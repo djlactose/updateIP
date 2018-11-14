6850e0712d567b5864472e586f51b19da872245e
Nick Hernandez <nickhernandez@djlactose.com>
2018-11-13 19:28:41 -0500

    Update dns to use 9.9.9.9

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ sed -i "s:key=\"\":key=$cf_key:g" /etc/updateIP/updateIP.conf
 sed -i "s:id=\"\":id=$cf_id:g" /etc/updateIP/updateIP.conf
 sed -i "s:zone=\"\":zone=$cf_zone:g" /etc/updateIP/updateIP.conf
 sed -i "s:subdomain=\"\":subdomain=$subdomain:g" /etc/updateIP/updateIP.conf
+sed -i "s:nameserver 192.168.65.1:nameserver 9.9.9.9:g" /etc/resolv.conf
 /bin/updateip
 crond -f
